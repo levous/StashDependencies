32be005ffff0d030a822b48fc172c3f5667fb405
Rusty Zarse <rzarse@bignerdranch.com>
2022-01-01 14:46:07 -0500

    initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "StashDependencies",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "StashDependencies",
+            targets: ["StashDependencies"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "StashDependencies",
+            url: "https://github.com/levous/StashDependencies/releases/download/1.0.0-alpha.1/StarWarsKit.xcframework.zip",
+            checksum: "b24b18fb3c11ca154242742ad9a18c3c67d4a75cb75b3e05d7dbb82cbd367227"
+        ),
+    ]
+)
